62349e69b3eaa67df42a50693753c0912e7f9238
Dvir Cohen <dvir906@gmail.com>
2022-12-10 09:58:56 +0200

    game logic

--- root/client/src/pages/GameBoard.js
+++ root/client/src/pages/GameBoard.js
@@ -1,20 +1,13 @@
 import React from "react";
+import TicTacToe from "../components/game/TicTacToe";
 
 import { useGameContext } from "../context/GameContext";
 const GameBoard = () => {
   const { initGameModal, handleInitGameModal } = useGameContext();
-  return (
-    <>
-      <button
-        onClick={() => {
-          handleInitGameModal();
-        }}
-        className="mt-10 bg-blue-500 py-2.5 px-5 rounded"
-      >
-        {" "}
-        Click
-      </button>
 
+  return (
+    <>{(initGameModal || <TicTacToe />)}
+      
     </>
   );
 };
